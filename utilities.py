242dad26f8f11b3279b36496155b9948c37dae5d
Krymeq <krymeq353@gmail.com>
2020-05-11 23:10:55 +0200

    Added raport, saving output to file and merged two notebooks...don't try this at home

--- utilities.py
+++ utilities.py
@@ -20,3 +20,14 @@ def change_resolution(img, factor):
 
 def threshold(img, threshpoint):
     return np.array([[1 if x >= threshpoint else 0 for x in y] for y in img])
+
+
+def apply_mask(img, mask):
+    print(np.shape(img))
+    print(np.shape(mask))
+    res = []
+    for i in range(len(img)):
+        res.append([])
+        for j in range(len(img[i])):
+            res[i].append(img[i][j] if mask[i][j] == 0 else 1)
+    return np.array(res)
